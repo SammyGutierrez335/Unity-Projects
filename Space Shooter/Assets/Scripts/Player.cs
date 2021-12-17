9f365c39db881d074c468b3560eaa8e1f49960ea
Sam Gutierrez <sammygutierrez335@gmail.com>
2021-12-17 10:45:40 -0600

    adds Player.cs file

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic; // part of the .net framework that allow us to
 using UnityEngine; // MonoBehaviour comes from this namespace
 
 public class Player : MonoBehaviour
+    // data type (int, float, bool, string)
 {
+    //public or private reference (public all other scripts can reference this variable...private, only Player can reference)
+    //public variables are also visible and adjustable in unity editor. 
+    //private methods will not be visible/adjustable unless they have [SerializeField] above.
+    public float speed = 3.5f;
+
+    //private variables by convention is named with a proceeding underscore _speed.
+    //private float _speed = 3.5
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +27,6 @@ public class Player : MonoBehaviour
         // transform.Translate(Vector3.right);
         
         //The distributed property newVector3(1, 0, 0) x 5 = newVector3(5, 0, 0) * real time
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }
